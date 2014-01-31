f3ee2f7da1ce65f78c35c448cb04319c3b5ebc4b
Greg Cotten <gregcotten@gmail.com>
2014-01-31 14:23:59 -0800

    removed an unnecessary file

--- src/test.py
+++
@@ -1,5 +0,0 @@
-import fileseq
-
-seqs = fileseq.findSequencesOnDisk("/Users/gregcotten/Temp/filesequence")
-for seq in seqs:
-	print seq.format("{dirname}{basename}[{range}]{extension} | Missing: {missing}", uniquePerFrameRange = False)
