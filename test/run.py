b7b238ba9173afbb8e095e2905088256684156d4
matthewkapfhammer <github@lookslikematt.com>
2014-11-23 14:43:53 +1300

    #7 Updated tests and added some for getPaddingChars.

--- test/run.py
+++ test/run.py
@@ -262,14 +262,20 @@ class TestPaddingFunctions(unittest.TestCase):
         self.assertEquals("1-25:8", fileseq.padFrameRange("1-25:8", 1))
         self.assertEquals("0001-0100:8", fileseq.padFrameRange("1-100:8", 4))
 
-
-class TestGetPaddingNumber(unittest.TestCase):
+    def testGetPaddingChars(self):
+        self.assertEqual('#', fileseq.getPaddingChars(4))
+        self.assertEqual('##', fileseq.getPaddingChars(8))
+        self.assertEqual('@', fileseq.getPaddingChars(1))
+        self.assertEqual('@@@@@@', fileseq.getPaddingChars(6))
 
     def testGetPaddingNumber(self):
         self.assertEqual(4, fileseq.getPaddingNumber('#'))
         self.assertEqual(8, fileseq.getPaddingNumber('##'))
         self.assertEqual(1, fileseq.getPaddingNumber('@'))
         self.assertEqual(6, fileseq.getPaddingNumber('@@@@@@'))
+
+
+
 
 if __name__ == '__main__':
     unittest.main(verbosity=2)
