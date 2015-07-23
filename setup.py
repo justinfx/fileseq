bb081c944a28d92b21a0ab7f487f615ea51fe77a
Justin Israel <justinisrael@gmail.com>
2015-07-24 10:43:45 +1200

    Add dynamic version module
    Signed-off-by: Justin Israel <justinisrael@gmail.com>

--- setup.py
+++ setup.py
@@ -1,13 +1,15 @@
 #!/usr/bin/env python
 
+import os
 from setuptools import setup, find_packages
 
+execfile(os.path.join(os.path.dirname(__file__), "src/fileseq/__version__.py"))
 
 descript = 'A Python library for parsing frame ranges and file ' \
                   'sequences based on a similar library found in Katana.'
 
 setup(name='Fileseq',
-      version='1.1.4',
+      version=__version__,
 
       package_dir = {'': 'src'},
       packages=find_packages('src'),
