aa88d02e856d06d23c3d083c96c5343cf5b285e1
Justin Israel <justinisrael@gmail.com>
2017-08-16 14:58:23 +1200

    update setup.py for new pypi.org

--- setup.py
+++ setup.py
@@ -2,9 +2,16 @@
 
 import os
 from setuptools import setup, find_packages
+from codecs import open
 
-execfile(os.path.join(os.path.dirname(__file__), "src/fileseq/__version__.py"))
+here = os.path.abspath(os.path.dirname(__file__))
 
+execfile(os.path.join(here, "src/fileseq/__version__.py"))
+
+# Get the long description from the README file
+with open(os.path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+    
 descript = 'A Python library for parsing frame ranges and file ' \
                   'sequences based on a similar library found in Katana.'
 
@@ -18,6 +25,19 @@ setup(name='Fileseq',
 
       author='Matt Chambers',
       author_email='yougotrooted@gmail.com',
+      
       url='https://github.com/sqlboy/fileseq',
-      description=descript
+      
+      description=descript,
+      long_description=long_description,
+      
+      license='MIT',
+
+      classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2.7',
+      ],
+      
+      keywords='vfx visual effects file sequence frames image',
      )
