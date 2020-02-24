828e17eff0826f1c42eb1ad8551a0ad355f63898
Justin Israel <justinisrael@gmail.com>
2020-02-25 09:08:39 +1300

    Clean up regex patterns with verbose mode

--- src/fileseq/constants.py
+++ src/fileseq/constants.py
@@ -16,8 +16,18 @@ PAD_MAP = {"#": 4, "@": 1}
 # Example: /film/shot/renders/bilbo_bty.1-100@.exr
 # Example: /film/shot/renders/bilbo_bty.1-100@@@@#.exr
 # Example: /film/shot/renders/bilbo_bty.1-100%04d.exr
-SPLIT_PATTERN = r"((?:[-\d][-:,xy\d]*)?)([{0}]+|%(?:\d)*d|\$F(?:\d)*)".format(''.join(PAD_MAP))
-SPLIT_RE = re.compile(SPLIT_PATTERN)
+# Example: /film/shot/renders/bilbo_bty.1-100$F4d.exr
+SPLIT_PATTERN = r"""
+    ((?:[-\d][-:,xy\d]*)?)  # range
+    (                       # pad format
+        [{0}]+              #   pad map characters
+        |
+        %(?:\d)*d           #   printf syntax pad format 
+        |
+        \$F(?:\d)*          #   Houdini syntax pad format
+    )  
+    """.format(''.join(PAD_MAP))
+SPLIT_RE = re.compile(SPLIT_PATTERN, re.X)
 
 # Regular expression pattern for matching padding against a printf syntax
 # padding string E.g. %04d
@@ -29,14 +39,46 @@ HOUDINI_SYNTAX_PADDING_PATTERN = r"\$F(\d)*\Z"
 HOUDINI_SYNTAX_PADDING_RE = re.compile(HOUDINI_SYNTAX_PADDING_PATTERN)
 
 # Regular expression pattern for matching file names on disk.
-DISK_PATTERN = r"^((?:.*[/\\])?)(.*?)(-?\d+)?((?:\.\w*[a-zA-Z]\w)*(?:\.[^.]+)?)$"
-DISK_RE = re.compile(DISK_PATTERN)
+DISK_PATTERN = r"""
+    ^
+    ((?:.*[/\\])?)            # dirname
+    (.*?)                     # basename
+    (-?\d+)?                  # frame
+    (                         # ext
+        (?:\.\w*[a-zA-Z]\w)*  #   optional leading alnum ext prefix (.foo.1bar)
+        (?:\.[^.]+)?          #   ext suffix
+    )
+    $
+    """
+DISK_RE = re.compile(DISK_PATTERN, re.X)
 
 # Regular expression pattern for matching frame set strings.
 # Examples: '1' or '1-100', '1-100x5', '1-100:5', '1-100y5', '1,2', etc.
-FRANGE_PATTERN = r"^(-?\d+)(?:-(-?\d+)(?:([:xy]{1})(-?\d+))?)?$"
-FRANGE_RE = re.compile(FRANGE_PATTERN)
+FRANGE_PATTERN = r"""
+    ^
+    (-?\d+)             # start frame
+    (?:                 # optional range
+        -               #   range delimiter
+        (-?\d+)         #   end frame
+        (?:             #   optional stepping
+            ([:xy]{1})  #     step format
+            (-?\d+)     #     step value
+        )?
+    )?
+    $
+    """
+FRANGE_RE = re.compile(FRANGE_PATTERN, re.X)
 
 # Regular expression for padding a frame range.
-PAD_PATTERN = r"(-?)(\d+)(?:(-)(-?)(\d+)(?:([:xy]{1})(\d+))?)?"
-PAD_RE = re.compile(PAD_PATTERN)
+PAD_PATTERN = r"""
+    (-?)(\d+)           # start frame
+    (?:                 # optional range
+        (-)             #   range delimiter
+        (-?)(\d+)       #   end frame
+        (?:             #   optional stepping
+            ([:xy]{1})  #     step format
+            (\d+)       #     step value
+        )?
+    )?
+    """
+PAD_RE = re.compile(PAD_PATTERN, re.X)
